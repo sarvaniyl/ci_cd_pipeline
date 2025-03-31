72ca0c5417e699af0edec32d1cf3df85d43409a9
sarvaniyl <yl.sarvani07@gmail.com>
2025-03-31 07:39:32 +0530

    added app folder

---
+++ app/main.py
@@ -0,0 +1,9 @@
+def hello_world():
+    return "Hello, Docker and GitHub Actions!"
+
+def sub(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    print(hello_world())
+    print(f"10 - 7  = {sub(10, 7)}")
